3b606b3119c7b61bdfcef9c93892ed08d0fa1ff3
Keithd2423 <144353516+Keithd2423@users.noreply.github.com>
2023-11-09 15:13:36 +0000

    messages

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('askButton').addEventListener('click', function(){
+let answer= ' ';
+let randomIndex = Math.floor(Math.random())
+
+if(randomIndex === 0){
+    answer = "it is certain";
+} else if (randomIndex === 1){
+    answer = "ask again later";
+} else if (randomIndex ===3)
+})
